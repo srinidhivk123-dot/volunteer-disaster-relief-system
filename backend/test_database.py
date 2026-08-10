005593534cbb3c30b14d3b1c3c0f34a2a8591116
SRINIDHI VK <srinidhivk123@gmail.com>
2026-08-10 10:41:24 +0530

    feat(backend): configure database connection

---
+++ backend/test_database.py
@@ -0,0 +1,8 @@
+from app.core.database import engine
+
+try:
+    with engine.connect() as connection:
+        print("Database connection successful!")
+except Exception as e:
+    print("Database connection failed!")
+    print(e)
